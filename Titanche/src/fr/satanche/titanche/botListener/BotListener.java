66e3faafbcff5607fb623b1d47f98fc570cddc77
Alexandre GIMENEZ PUIG <alexandre.gimenez-puig@kurmi-software.com>
2017-09-18 14:03:03 +0200

    Bot Commands : Setup core
    Using NeutronStars tutorial, setup a command manager for our bot.

---
+++ Titanche/src/fr/satanche/titanche/botListener/BotListener.java
@@ -0,0 +1,32 @@
+package fr.satanche.titanche.botListener;
+
+
+import fr.satanche.titanche.command.core.CommandFactory;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+public class BotListener implements EventListener {
+
+	private final CommandFactory commandFactory;
+	
+	public BotListener(CommandFactory commandFactory){
+		this.commandFactory = commandFactory;
+	}
+	
+	@Override
+	public void onEvent(Event event){
+		System.out.println(event.getClass().getSimpleName());
+		if(event instanceof MessageReceivedEvent) onMessage((MessageReceivedEvent) event);
+	}
+	
+	public void onMessage(MessageReceivedEvent event){
+		if(event.getAuthor().equals(event.getJDA().getSelfUser())) return;
+		String message = event.getMessage().getContent();
+		if(message.startsWith(commandFactory.getTag())){
+			message = message.replaceFirst(commandFactory.getTag(), "");
+			commandFactory.commandUser(event.getAuthor(), message, event.getMessage());
+		}
+		
+	}
+}
